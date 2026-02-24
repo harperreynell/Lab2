75e854476f6ffe39566ff18c02f1d4702a9c1ed7
harperreynell <sentinel.whoami@gmail.com>
2026-02-24 13:15:05 +0200

    Single thread maximum value search

--- main.cpp
+++ main.cpp
@@ -9,9 +9,17 @@ int count_bigger(const std::vector<long> &vec, const int n) {
     return cnt;
 }
 
+long max(const std::vector<long> &vec) {
+    long max = vec[0];
+    for (const long i : vec) {
+        if (i > max) max = i;
+    }
+    return max;
+}
+
 int main() {
-    std::vector<long> vec(100);
-    for (int i = 0; i < 100; i++) {
+    std::vector<long> vec(1000);
+    for (int i = 0; i < 1000; i++) {
         std::random_device rd;
         std::mt19937 gen(rd());
         std::uniform_int_distribution<int> distrib(-1000, 1000);
@@ -19,6 +27,6 @@ int main() {
 
     }
     std::cout << count_bigger(vec, 500) << std::endl;
-
+    std::cout << max(vec) << std::endl;
     return 0;
 }
