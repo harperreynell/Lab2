1145726381196fbe4d2719b8071377be46d8db49
harperreynell <sentinel.whoami@gmail.com>
2026-02-24 13:12:26 +0200

    Single thread, simplest search for values more than N

--- main.cpp
+++ main.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
-#include <thread>
-#include <atomic>
+#include <random>
+
+int count_bigger(const std::vector<long> &vec, const int n) {
+    int cnt = 0;
+    for (const long i : vec) {
+        if (i > n) cnt++;
+    }
+    return cnt;
+}
 
 int main() {
-    std::atomic<int> max;
-
-
     std::vector<long> vec(100);
     for (int i = 0; i < 100; i++) {
-        vec[i] = random()%1000;
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<int> distrib(-1000, 1000);
+        vec[i] = distrib(gen);
+
     }
-    for (int i = 0; i < 100; i++) {
-        std::cout << vec[i] << ' ';
-    }
-    std::cout << '\n';
+    std::cout << count_bigger(vec, 500) << std::endl;
 
     return 0;
 }
