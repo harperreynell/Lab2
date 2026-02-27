4e2d396c74c27bac4466a0611d6ad22b580c5564
harperreynell <sentinel.whoami@gmail.com>
2026-02-27 23:58:15 +0200

    Blocking algorithm

--- main.cpp
+++ main.cpp
@@ -1,20 +1,36 @@
 #include <iostream>
 #include <random>
 
-std::pair<int, int> count_bigger(const std::vector<long> &vec, const int n) {
+std::pair<int, int> calculate(const std::vector<long> &vec, const int n) {
     int cnt = 0;
     long max = vec[0];
     for (const long i : vec) {
-        if (i > max) {
-            max = i;
-        }
+        if (i > max) max = i;
         if (i > n) cnt++;
     }
     return std::make_pair(cnt, max);
 }
 
+std::mutex mtx;
+std::pair<int, int> calculate_blocking(const std::vector<long> &vec, const int n, int &cnt, int &max) {
+    int local_count = 0;
+    int local_max = std::numeric_limits<int>::min();
+
+    for (const long i : vec) {
+        if (i > max) local_max = i;
+        if (i > n) local_count++;
+    }
+
+    std::lock_guard lock(mtx);
+    cnt += local_count;
+    if (local_max > max) max = local_max;
+
+    return std::make_pair(cnt, max);
+}
+
 int main() {
-    std::vector<long> vec(1000);
+    int n = 500;
+    std::vector<long> vec(500);
     for (int i = 0; i < 1000; i++) {
         std::random_device rd;
         std::mt19937 gen(rd());
@@ -22,8 +38,24 @@ int main() {
         vec[i] = distrib(gen);
     }
 
-    std::pair<int,int> result = count_bigger(vec, 500);
-    std::cout << result.first << std::endl;
-    std::cout << result.second << std::endl;
+    std::cout << "Single thread no blocking:\n";
+    auto start = std::chrono::high_resolution_clock::now();
+    std::pair<int,int> result = calculate(vec, n);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "More than " << n << " elements: " << result.first << std::endl;
+    std::cout << "Max element: " << result.second << std::endl;
+    std::cout << "Time: " << elapsed.count() << "s" << std::endl;
+
+    int cnt = 0;
+    int max = std::numeric_limits<int>::min();
+    start = std::chrono::high_resolution_clock::now();
+    result = calculate_blocking(vec, n, cnt, max);
+    end = std::chrono::high_resolution_clock::now();
+    elapsed = end - start;
+    std::cout << "\nWith blocking:\n";
+    std::cout << "More than " << n << " elements: " << result.first << std::endl;
+    std::cout << "Max element: " << result.second << std::endl;
+    std::cout << "Time: " << elapsed.count() << "s" << std::endl;
     return 0;
 }
